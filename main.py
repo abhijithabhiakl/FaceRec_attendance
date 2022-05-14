73d093839bb3e0887dc8d87bf9cf897d517b58ac
ash6327 <67222995+ash6327@users.noreply.github.com>
2022-05-14 11:01:00 +0530

    face recognition phase done

--- main.py
+++ main.py
@@ -4,19 +4,23 @@ import sqlite3
 import numpy as np
 import face_recognition
 
-sya = face_recognition.load_image_file('image assets\sya.jpeg')
-ada = face_recognition.load_image_file('image assets\sada.jpeg')
+sya = face_recognition.load_image_file('image assets\Shyam Saseendran.jpeg')
+ada = face_recognition.load_image_file('image assets\Adarsh Sreenivasan.jpeg')
 sya = cv2.cvtColor(sya, cv2.COLOR_BGR2RGB)
 ada = cv2.cvtColor(ada, cv2.COLOR_BGR2RGB)
 
 facloc = face_recognition.face_locations(sya)[0]
 encodesya = face_recognition.face_encodings(sya)[0]
-cv2.rectangle(sya, (facloc[3], facloc[0]), (facloc[1], facloc[2]), (0, 0, 0), 2)
+cv2.rectangle(sya, (facloc[3], facloc[0]), (facloc[1], facloc[2]), (0, 255, 0), 2)
 
 facloc1 = face_recognition.face_locations(ada)[0]
 encodeada = face_recognition.face_encodings(ada)[0]
-cv2.rectangle((ada, (facloc1[3], facloc1[0]),(facloc1[1],facloc1[0]), (0,0,0), 2)
+cv2.rectangle(ada, (facloc1[3], facloc1[0]), (facloc1[1], facloc1[2]), (0, 255, 0), 2)
 
+results = face_recognition.compare_faces(encodesya, [encodeada])
+resdis = face_recognition.face_distance(encodesya, [encodeada])
+print(results)
+print(resdis)
 
 cv2.imshow("syam", sya)
 cv2.imshow("ada", ada)
